44f38bb110cfbeaabb3c93c46b3ff173c75c2bbe
JBBSoftech <jbbsoftech@gmail.com>
2025-11-17 15:57:02 +0530

    Update Appifyours/frontend/lib/main.dart

--- Appifyours/frontend/lib/main.dart
+++ Appifyours/frontend/lib/main.dart
@@ -829,7 +829,7 @@ class _HomePageState extends State<HomePage> {
                         
                         const SizedBox(width: 8),
                         Text(
-                          'deyyk',
+                          'kaniko',
                           style: const TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
